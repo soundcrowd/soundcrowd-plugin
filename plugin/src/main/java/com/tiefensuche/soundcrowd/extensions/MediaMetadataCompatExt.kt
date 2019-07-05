03997e8fc9a265e4137ea559011380b3f35a22b5
tiefensuche <10889432+tiefensuche@users.noreply.github.com>
2019-07-05 03:04:39 +0200

    Added conversion methods from JSON to MediaMetadataCompat and vice versa

--- plugin/src/main/java/com/tiefensuche/soundcrowd/extensions/MediaMetadataCompatExt.kt
+++ plugin/src/main/java/com/tiefensuche/soundcrowd/extensions/MediaMetadataCompatExt.kt
@@ -4,6 +4,10 @@
 
 package com.tiefensuche.soundcrowd.extensions
 
+import android.support.v4.media.MediaMetadataCompat
+import org.json.JSONException
+import org.json.JSONObject
+
 /**
  * Extensions for MediaMetadataCompat
  */
@@ -13,9 +17,34 @@ object MediaMetadataCompatExt {
     const val METADATA_KEY_WAVEFORM_URL = "WAVEFORM_URL"
     const val METADATA_KEY_TYPE = "TYPE"
     const val METADATA_KEY_SOURCE = "SOURCE"
+    const val METADATA_KEY_PREFERENCES = "PREFERENCES"
 
     // values for METADATA_KEY_TYPE
     enum class MediaType {
         MEDIA, COLLECTION, STREAM
+    }
+
+    @Throws(JSONException::class)
+    fun fromJson(json: JSONObject): MediaMetadataCompat {
+        val builder = MediaMetadataCompat.Builder()
+        val keys = json.keys()
+        while (keys.hasNext()) {
+            val key = keys.next()
+            val value = json.get(key)
+            when (value) {
+                is String -> builder.putString(key, value)
+                is Long -> builder.putLong(key, value)
+                is Int -> builder.putLong(key, value.toLong())
+            }
+        }
+        return builder.build()
+    }
+
+    fun toJSON(metadata: MediaMetadataCompat): JSONObject {
+        val json = JSONObject()
+        metadata.keySet().forEach { key ->
+            json.put(key, metadata.bundle.get(key))
+        }
+        return json
     }
 }
