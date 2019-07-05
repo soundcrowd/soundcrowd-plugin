32eeadd3662c60cda900984a9ce6fa1f901b0e87
tiefensuche <10889432+tiefensuche@users.noreply.github.com>
2019-07-05 03:09:38 +0200

    Slightly reworked webrequests

--- plugin/src/main/java/com/tiefensuche/soundcrowd/extensions/WebRequests.kt
+++ plugin/src/main/java/com/tiefensuche/soundcrowd/extensions/WebRequests.kt
@@ -7,7 +7,6 @@ package com.tiefensuche.soundcrowd.extensions
 import java.io.BufferedReader
 import java.io.IOException
 import java.net.URL
-import java.net.UnknownHostException
 import javax.net.ssl.HttpsURLConnection
 
 object WebRequests {
@@ -26,50 +25,42 @@ object WebRequests {
      */
     @Throws(IOException::class)
     fun get(siteUrl: String): String {
-        val con = URL(siteUrl).openConnection() as HttpsURLConnection
-        return request(con)
+        return request(createConnection(siteUrl))
     }
 
     fun post(siteUrl: String, content: String): String {
-        val con = URL(siteUrl).openConnection() as HttpsURLConnection
+        val con = createConnection(siteUrl)
         con.doOutput = true
         con.requestMethod = "POST"
         con.outputStream.write(content.toByteArray())
         return request(con)
     }
 
+    @Throws(IOException::class)
+    fun createConnection(url: String): HttpsURLConnection {
+        val con = URL(url).openConnection() as? HttpsURLConnection ?: throw IOException()
+        con.connectTimeout = 30 * 1000 // 30s
+        con.readTimeout = 30 * 1000 // 30s
+        con.setRequestProperty("User-Agent", USER_AGENT)
+
+        if (cookies.isNotEmpty()) {
+            con.setRequestProperty("Cookie", cookies)
+        }
+
+        return con
+    }
+
     /**
      * Common functionality between download(String url) and download(String url, String language)
      */
-    @Throws(IOException::class)
+    @Throws(HttpException::class)
     fun request(con: HttpsURLConnection): String {
-        val response: String
-        try {
-            con.connectTimeout = 30 * 1000 // 30s
-            con.readTimeout = 30 * 1000 // 30s
-            con.setRequestProperty("User-Agent", USER_AGENT)
-
-            if (cookies.isNotEmpty()) {
-                con.setRequestProperty("Cookie", cookies)
-            }
-
-            response = con.inputStream.bufferedReader().use(BufferedReader::readText)
-
-        } catch (e: UnknownHostException) { //thrown when there's no internet connection
-            throw IOException("unknown host or no network", e)
-        } catch (e: Exception) {
-            /*
-            * HTTP 429 == Too Many Request
-            * Receive from Youtube.com = ReCaptcha challenge request
-            * See : https://github.com/rg3/youtube-dl/issues/5138
-            */
-            if (con.responseCode == 429) {
-                throw IOException("reCaptcha Challenge requested")
-            }
-
-            throw IOException(con.responseCode.toString() + " " + con.responseMessage, e)
+        if (con.responseCode < 400) {
+            return con.inputStream.bufferedReader().use(BufferedReader::readText)
+        } else {
+            throw HttpException(con.responseCode, con.errorStream.bufferedReader().use(BufferedReader::readText))
         }
-
-        return response
     }
+
+    class HttpException(val code: Int, message: String?) : IOException(message)
 }
